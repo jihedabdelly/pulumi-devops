5d142811e3f00366320b0ef104f3e4064dfc2672
jihedabdelly <jihednew7@gmail.com>
2024-05-04 00:35:26 +0100

    import bucket creation into index

--- index.ts
+++ index.ts
@@ -1,3 +1,9 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 
+import FmBucket from "./resources/bucket";
+
+new FmBucket({
+    Name: "example",
+    Product: "devops-with-pulumi"
+})
--- resources/bucket.ts
+++ resources/bucket.ts
@@ -1,5 +1,5 @@
-import pulumi, { getStack } from "@pulumi/pulumi";
-import aws from "@pulumi/aws";
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
 
 
 type FmBucketArgs = {
@@ -7,12 +7,12 @@ type FmBucketArgs = {
     Product: string
 }
 
-class FmBucket extends pulumi.ComponentResource {
-    constructor(args: FmBucketArgs, opts: pulumi.ComponentResourceOptions) {
+export default class FmBucket extends pulumi.ComponentResource {
+    constructor(args: FmBucketArgs, opts?: pulumi.ComponentResourceOptions) {
         const resourceName = `${args.Product}-${args.Name}`
         super("pkg:index:FmBucket", resourceName, {}, opts);
 
-        const stack = getStack();
+        const stack = pulumi.getStack();
 
         const bucketName = `${resourceName}-${stack}`
 
@@ -35,4 +35,3 @@ class FmBucket extends pulumi.ComponentResource {
 
     
 }
-

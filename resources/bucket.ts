b7fc73339a29ddb047188b9a7c7d9155fd053170
jihedabdelly <jihednew7@gmail.com>
2024-05-03 23:58:09 +0100

    typed bucket arguments

--- resources/bucket.ts
+++ resources/bucket.ts
@@ -1,8 +1,27 @@
 import pulumi from "@pulumi/pulumi";
+import aws from "@pulumi/aws";
+
+
+type FmBucketArgs = {
+    Name: string,
+    Product: string
+}
 
 class FmBucket extends pulumi.ComponentResource {
-    constructor(name: string, opts: pulumi.ComponentResourceOptions) {
+    constructor(args: FmBucketArgs, opts: pulumi.ComponentResourceOptions) {
+        const name = `${args.Product}-${args.Name}`
         super("pkg:index:FmBucket", name, {}, opts);
+
+        const b = new aws.s3.Bucket("b", {
+            bucket: "my-tf-test-bucket",
+            acl: aws.s3.CannedAcl.Private,
+            tags: {
+                Name: "My bucket",
+                Environment: "Dev",
+            },
+        });
     }
+
+    
 }
 

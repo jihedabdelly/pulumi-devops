50efbc99c5d5546281474f3d3755dcd888f67116
jihedabdelly <jihednew7@gmail.com>
2024-05-04 14:21:39 +0100

    automating multi bucket creation

--- index.ts
+++ index.ts
@@ -1,6 +1,15 @@
 import FmBucket from "./resources/bucket";
 
-new FmBucket({
-    Name: "example",
-    Product: "devops-with-pulumi"
-})
+const bucketList: string[] = [
+    "example-1",
+    "example-2"
+]
+
+for(const bucket in bucketList) {
+    new FmBucket({
+        Name: bucket,
+        Product: "devops-with-pulumi"
+    })
+}
+
+
